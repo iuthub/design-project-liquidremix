09c9e6544530045b191f02881103193d16ff09c3
azamat98 <mr.azik98@mail.ru>
2019-05-01 22:31:36 +0500

    added the post model including relationship to other models, basic create post feature is working=>successfully adding to the databse

---
+++ app/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ['title','category','description'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User','user_id');
+    }
+}
