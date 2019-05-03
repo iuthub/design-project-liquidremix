d84d5df1572ee461396b6ef1f2a3af18695455d9
SAIDAKBAR123 <47180339+SAIDAKBAR123@users.noreply.github.com>
2019-05-03 15:03:31 +0500

    added ui liking icon

--- resources/views/home/wishes.blade.php
+++ resources/views/home/wishes.blade.php
@@ -12,49 +12,23 @@
     
                 <h2 class="">Liked products</h2>
                 <p>here you can know what you were looking for</p>
-              <div class="card mb-3 boxing" style="max-width: 740px;">
-                <div class="row no-gutters">
-                  <div class="col-md-4">
-                    <img src="http://cdn.motorpage.ru/Photos/800/184.jpg" class="card-img img-product" alt="...">
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">Nexia<span id="value-product-list">1,200,000 so'm</span></h5>
-                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                      <p class="card-text"><small class="text-muted"><i class="fas fa-thumbtack"></i>Mirzo Ulugbek rg, Olimlar street</small><small><i style="color:burlywood; padding-left: 10%;" class="fas fa-history"></i>Time: 29/02/2019</small><span style="float:right; font-size:15pt;"><i class="far fa-heart"></i></span></p>
-                    </div>
-                  </div>
-                </div>
-              </div> 
-              <div class="card mb-3 boxing" style="max-width: 740px;">
-                    <div class="row no-gutters">
-                      <div class="col-md-4">
-                        <img src="http://cdn.motorpage.ru/Photos/800/184.jpg" class="card-img img-product" alt="...">
-                      </div>
-                      <div class="col-md-8">
-                        <div class="card-body">
-                          <h5 class="card-title">Nexia<span id="value-product-list">1,200,000 so'm</span></h5>
-                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                          <p class="card-text"><small class="text-muted"><i class="fas fa-thumbtack"></i>Mirzo Ulugbek rg, Olimlar street</small><small><i style="color:burlywood; padding-left: 10%;" class="fas fa-history"></i>Time: 29/02/2019</small><span style="float:right; font-size:15pt;"><i class="far fa-heart"></i></span></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div> 
-    
-                  <div class="card mb-3  boxing" style="max-width: 740px;">
-                      <div class="row no-gutters">
-                        <div class="col-md-4">
-                          <img src="https://www.gigaset.com/media/catalog/product/0/3/03_smartphone_gigaset_gs370_jetblack_tl_topview_lake.jpg" class="card-img img-product" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                          <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <div class="card mb-3 boxing" style="max-width: 740px;">
+                        <div class="row no-gutters">
+                          <div class="col-md-4">
+                            <img src="http://cdn.motorpage.ru/Photos/800/184.jpg" class="card-img img-product" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h5 class="card-title">Nexia<span id="value-product-list">1,200,000 so'm</span></h5>
+                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                              <p class="card-text"><small class="text-muted"><i class="fas fa-thumbtack"></i>Mirzo Ulugbek rg, Olimlar street</small><small><i style="color:burlywood; padding-left: 10%;" class="fas fa-history"></i>Time: 29/02/2019</small><span class="like" style="float:right; font-size:15pt;"><i id="like-heart" class="far fa-heart"></i></span></p>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </div> 
+                      </div> 
+            
+    
+                 
     
     
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,9 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/scrolling-nav.css')}}" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="{{asset('js/scrolling-nav.js')}}"></script>
+
 </head>
 <body>
     <div id="app">
