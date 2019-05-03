98bbca55ea5dc04a345a2b03e9c58bd76565a4a1
azamat98 <mr.azik98@mail.ru>
2019-05-03 12:52:10 +0500

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,11 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
     <title>{{ config('app.name', 'melon') }}</title>
-    
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
