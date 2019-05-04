92ce2195a4aacdaf0c9b31d29f9755e09900fac6
ramon17 <abdurahmon-99@mail.ru>
2019-05-05 00:18:00 +0500

    conflict resolve

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Custom\ExchangeRate;
+use App\Post;
 class LandingPageController extends Controller
 {
     public function getIndex()
     {
-        return view('index',['rate'=>ExchangeRate::getRates()]);
+    	$posts = Post::all();
+    	
+        return view('index',['rate'=>ExchangeRate::getRates()])->withPosts($posts);
     }
 
     public function getCategory($id)
--- app/Post.php
+++ app/Post.php
@@ -6,10 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title','category','description'];
+    protected $fillable = ['title','category','description','price','status'];
 
     public function user()
     {
         return $this->belongsTo('App\User','user_id');
+    }
+    public function photos()
+    {
+        return $this->hasMany('App\PostPhoto');
     }
 }
