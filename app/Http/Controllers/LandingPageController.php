c270a07ce48644c7b3b21b58108fe0fa17f93b99
SAIDAKBAR123 <47180339+SAIDAKBAR123@users.noreply.github.com>
2019-05-05 14:54:00 +0500

    added link for likes

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Custom\ExchangeRate;
 use App\Post;
+use App\Like;
 class LandingPageController extends Controller
 {
     public function getIndex()
