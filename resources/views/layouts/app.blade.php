02fb22c8485caad26846d4ef5c991322d7dd4fac
SAIDAKBAR123 <47180339+SAIDAKBAR123@users.noreply.github.com>
2019-05-05 19:21:46 +0500

    added filter for view index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,7 +105,9 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a href="{{route('home.index')}}" class="dropdown-item">My account</a>
+                                <a href="{{route('home.index')}}" class="dropdown-item">Successed</a>
+                                <a href="{{route('home.index')}}" class="dropdown-item">Rejected</a>
+                                <a href="{{route('home.index')}}" class="dropdown-item">Waiting..</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
